974f2f5313e5678f7be08eaa888f17b10e4069ba
trainingtswe <matteoasteria1d@gmail.com>
2026-08-15 19:29:00 +0200

    Add: sql-sellers-with-no-sales - submission-8

---
+++ SQL/sql-sellers-with-no-sales/submission-8.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+SELECT DISTINCT seller_name
+FROM seller s 
+WHERE s.seller_id NOT IN (
+    SELECT seller_id 
+    FROM orders
+    WHERE EXTRACT (YEAR FROM sale_date) = 2020
+)
+ORDER BY seller_name;
+
+--Understand clearly why there is no need of a join in this query

d66eb4858539b2bb4cadb71d095059570192f9d9
trainingtswe <matteoasteria1d@gmail.com>
2026-08-22 17:06:48 +0200

    Add: sql-number-of-calls-between-two-persons - submission-2

---
+++ SQL/sql-number-of-calls-between-two-persons/submission-2.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+
+SELECT 
+LEAST(from_id, to_id) AS person1,
+GREATEST(to_id, from_id) AS person2,
+COUNT(*) AS call_count,
+SUM(duration) AS total_duration 
+FROM calls
+GROUP BY person1, person2

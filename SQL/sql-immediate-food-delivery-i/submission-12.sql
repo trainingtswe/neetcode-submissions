e257af4e185367a226a550077335028c16ba3e95
trainingtswe <matteoasteria1d@gmail.com>
2026-08-22 16:43:29 +0200

    Add: sql-immediate-food-delivery-i - submission-12

---
+++ SQL/sql-immediate-food-delivery-i/submission-12.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+SELECT 
+ROUND(SUM(
+CASE WHEN order_date=customer_pref_delivery_date THEN 1
+ELSE 0 END)*100.0/COUNT(delivery_id),2) AS immediate_percentage
+FROM delivery

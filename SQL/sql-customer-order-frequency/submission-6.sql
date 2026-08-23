35eb7074292ce936a9d51f1bcb3695d74162c591
trainingtswe <matteoasteria1d@gmail.com>
2026-08-23 20:39:30 +0200

    Add: sql-customer-order-frequency - submission-6

---
+++ SQL/sql-customer-order-frequency/submission-6.sql
@@ -0,0 +1,14 @@
+-- Write your query below
+
+SELECT 
+c.customer_id,
+c.name
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN product p ON o.product_id = p.product_id
+GROUP BY c.customer_id, c.name
+HAVING  
+SUM (CASE
+WHEN o.order_date BETWEEN '2020-06-01' AND '2020-06-30' THEN o.quantity*p.price ELSE 0 END) >= 100 AND
+SUM (CASE
+WHEN o.order_date BETWEEN '2020-07-01' AND '2020-07-31' THEN o.quantity*p.price ELSE 0 END) >=100;
